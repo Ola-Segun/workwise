3717987480b505ecfc480cbf0782ecaea143fd23
olaniyan segun <olaniyanpaul012@gmail.com>
2024-04-19 14:52:40 -1100

    Updating views

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 @include('partials._hero')
+@include('partials._search')
     
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
@@ -43,12 +44,7 @@
     </div>
   </div>
 
-{{-- <h2>
-    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
-</h2>
-<p>
-    {{ $listing['description'] }}
-</p> --}}
+
 @endforeach
     
 @else
